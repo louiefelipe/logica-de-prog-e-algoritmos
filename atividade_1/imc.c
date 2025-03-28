336850df53ef16d0aeabc7d974761bd6578a8e21
louiefelipe <louiefelipe288@gmail.com>
2025-03-28 10:57:38 -0300

    atividade_1

--- atividade_1/imc.c
+++ atividade_1/imc.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include<stdlib.h>
 int main(int argc, char* argv[]){
-    printf("Calculadora de IMC\n");
-
+    printf("\tCalculadora de IMC\n");
+system("pause");
 float peso, altura, IMC;
 
 printf("Digite a altura: ");
     scanf("%f", &altura);
     printf("Digite o peso: ");
     scanf("%f", &peso);
-    printf("A altura eh: %.2f\n", altura);
-    printf("O peso eh: %f\n",peso);
+    printf("A altura e: %.2f\n", altura);
+    printf("O peso e: %f\n",peso);
 
 IMC = peso/(altura*altura);
-printf("O IMC eh de: %.2f\n", IMC);
+printf("O IMC e de: %.2f\n", IMC);
 
+system("pause");
 return 0;
 
 }
