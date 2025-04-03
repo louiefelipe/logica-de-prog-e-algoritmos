efd71d2a28ec5846baac658c5a829e38fda0ec0a
louiefelipe <louiefelipe288@gmail.com>
2025-04-03 11:46:53 -0300

    atividade1

--- atividade_1/imc.c
+++ atividade_1/imc.c
@@ -9,11 +9,34 @@ printf("Digite a sua altura: ");
     scanf("%f", &altura);
     printf("Digite o seu peso: ");
     scanf("%f", &peso);
-    printf("A altura e: %.2f\n", altura);
-    printf("O peso e: %.2f\n",peso);
+system("pause");
 
 IMC = peso/(altura*altura);
 printf("Seu IMC e de: %.2f\n", IMC);
+
+if(IMC <= 18.5){
+printf("Classificacao: Baixo Peso\n");
+}
+
+if(IMC > 18.5 && IMC <= 24.9){
+printf("Classificacao: Normal\n");
+}
+
+if(IMC > 24.9 && IMC <= 29.9){
+printf("Classificacao: Sobrepeso\n");
+}
+
+if(IMC > 29.9 && IMC <= 34.9){
+printf(" Classificacao: Obesidade I\n");
+}
+
+if(IMC > 34.9 && IMC <= 39.9){
+printf("Classificacao: Obesidade II\n");
+}
+
+if(IMC > 39.9){
+printf("Classificacao: Obesidade III\n");
+}
 
 system("pause");
 return 0;
